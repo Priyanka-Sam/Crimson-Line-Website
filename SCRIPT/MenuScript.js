a0869c393d844d695f4f04c5c35c05bd9e2b30f5
Priyanka-Sam <46075615+Priyanka-Sam@users.noreply.github.com>
2020-06-23 17:31:05 +0530

    added the scripts/styles

---
+++ SCRIPT/MenuScript.js
@@ -0,0 +1,228 @@
+// JavaScript Document
+
+function OpenNav()
+{
+	document.getElementById("MENU").style.width="400px";
+}
+
+function CloseNav()
+{
+	document.getElementById("MENU").style.width="0";
+	
+}
+function MoveImage()
+{
+	document.getElementById("image").style.left="6%";
+	document.getElementById("image").style.opacity="0.9";
+}
+function MoveImageBack()
+{
+	document.getElementById("image").style.left="60%";
+	document.getElementById("image").style.opacity="1";
+}
+
+
+function BannerHover()
+{
+	document.getElementById("banner").style.opacity="1";
+}
+
+function CardPop1()
+{
+	document.getElementById("card1").style.zIndex="4";
+	document.getElementById("card1").style.width="600px";
+	document.getElementById("card1").style.height="400px";
+	document.getElementById("card1").style.fontSize="128px";
+	document.getElementById("card1").style.top="22%";
+	document.getElementById("card1").style.left="18%";
+	document.getElementById("card2").style.left="28%";
+	document.getElementById("card3").style.left="38%";
+	document.getElementById("card4").style.left="48%";
+	document.getElementById("card5").style.left="58%";
+	
+}
+function CardPopin1()
+{
+	document.getElementById("card1").style.zIndex="1";
+	document.getElementById("card1").style.width="300px";
+	document.getElementById("card1").style.height="200px";
+	document.getElementById("card1").style.fontSize="64px";
+	document.getElementById("card1").style.top="25%";
+	document.getElementById("card1").style.left="20%";
+	document.getElementById("card2").style.left="30%";
+	document.getElementById("card3").style.left="40%";
+	document.getElementById("card4").style.left="50%";
+	document.getElementById("card5").style.left="60%";
+	
+}
+function CardPop2()
+{
+	document.getElementById("card2").style.zIndex="4";
+	document.getElementById("card2").style.width="600px";
+	document.getElementById("card2").style.height="400px";
+	document.getElementById("card2").style.fontSize="128px";
+	document.getElementById("card2").style.top="22%";
+	document.getElementById("card1").style.left="18%";
+	document.getElementById("card2").style.left="28%";
+	document.getElementById("card3").style.left="38%";
+	document.getElementById("card4").style.left="48%";
+	document.getElementById("card5").style.left="58%";
+}
+function CardPopin2()
+{
+	document.getElementById("card2").style.zIndex="2";
+	document.getElementById("card2").style.width="300px";
+	document.getElementById("card2").style.height="200px";
+	document.getElementById("card2").style.fontSize="64px";
+	document.getElementById("card2").style.top="25%";
+	document.getElementById("card1").style.left="20%";
+	document.getElementById("card2").style.left="30%";
+	document.getElementById("card3").style.left="40%";
+	document.getElementById("card4").style.left="50%";
+	document.getElementById("card5").style.left="60%";
+	
+	
+}
+function CardPop3()
+{
+	document.getElementById("card3").style.zIndex="4";
+	document.getElementById("card3").style.width="600px";
+	document.getElementById("card3").style.height="400px";
+	document.getElementById("card3").style.fontSize="128px";
+	document.getElementById("card3").style.top="22%";
+	document.getElementById("card3").style.left="35%";
+	
+}
+function CardPopin3()
+{
+	document.getElementById("card3").style.zIndex="3";
+	document.getElementById("card3").style.width="300px";
+	document.getElementById("card3").style.height="200px";
+	document.getElementById("card3").style.fontSize="64px";
+	document.getElementById("card3").style.top="25%";
+	document.getElementById("card3").style.left="40%";
+	
+	
+	
+}
+function CardPop4()
+{
+	document.getElementById("card4").style.zIndex="4";
+	document.getElementById("card4").style.width="600px";
+	document.getElementById("card4").style.height="400px";
+	document.getElementById("card4").style.fontSize="128px";
+	document.getElementById("card4").style.top="22%";
+	document.getElementById("card1").style.left="22%";
+	document.getElementById("card2").style.left="32%";
+	document.getElementById("card3").style.left="42%";
+	document.getElementById("card4").style.left="52%";
+	document.getElementById("card5").style.left="62%";
+}
+function CardPopin4()
+{
+	document.getElementById("card4").style.zIndex="2";
+	document.getElementById("card4").style.width="300px";
+	document.getElementById("card4").style.height="200px";
+	document.getElementById("card4").style.fontSize="64px";
+	document.getElementById("card4").style.top="25%";
+	document.getElementById("card1").style.left="20%";
+	document.getElementById("card2").style.left="30%";
+	document.getElementById("card3").style.left="40%";
+	document.getElementById("card4").style.left="50%";
+	document.getElementById("card5").style.left="60%";
+	
+	
+}
+function CardPop5()
+{
+	document.getElementById("card5").style.zIndex="4";
+	document.getElementById("card5").style.width="600px";
+	document.getElementById("card5").style.height="400px";
+	document.getElementById("card5").style.fontSize="128px";
+	document.getElementById("card5").style.top="22%";
+	document.getElementById("card1").style.left="22%";
+	document.getElementById("card2").style.left="32%";
+	document.getElementById("card3").style.left="42%";
+	document.getElementById("card4").style.left="52%";
+	document.getElementById("card5").style.left="62%";
+}
+function CardPopin5()
+{
+	document.getElementById("card5").style.zIndex="1";
+	document.getElementById("card5").style.width="300px";
+	document.getElementById("card5").style.height="200px";
+	document.getElementById("card5").style.fontSize="64px";
+	document.getElementById("card5").style.top="25%";
+	document.getElementById("card1").style.left="20%";
+	document.getElementById("card2").style.left="30%";
+	document.getElementById("card3").style.left="40%";
+	document.getElementById("card4").style.left="50%";
+	document.getElementById("card5").style.left="60%";
+	
+	
+}
+function focusMap()
+{
+	document.getElementById("map").style.opacity="1";
+	document.getElementById("map").style.width="1400px";
+	document.getElementById("map").style.height="500px";
+	document.getElementById("map").style.left="0px";
+}
+function blurMap()
+{
+	document.getElementById("map").style.opacity="0.5";
+	document.getElementById("map").style.width="1200px";
+	document.getElementById("map").style.height="300px";
+	document.getElementById("map").style.left="100px";
+}
+
+function moveplate4()
+{
+	document.getElementById("plate4").style.left="200px";
+}
+
+function moveplate3()
+{
+document.getElementById("plate3").style.top="460px";
+}
+
+function moveplate2()
+{
+	
+		document.getElementById("plate2").style.left="200px";
+	document.getElementById("plate2").style.top="460px";
+}
+
+function CardReveal1()
+{
+	document.getElementById("card1").style.height="360px";
+	document.getElementById("card1").innerHTML="TOLL FREE NUMBERS<br><br>0126886645<br><br>0145367934<br><br>4503452053";
+           
+}
+function CardReveal2()
+{
+	document.getElementById("card2").style.height="360px";
+	document.getElementById("card2").innerHTML="PHONE NUMBERS <br><br> 9723800177<br><br>9494356912<br><br>9734250985";
+}
+function CardReveal3()
+{
+	document.getElementById("card3").style.height="360px";
+	document.getElementById("card3").innerHTML="E-MAIL<br> <br>US<br><br>AT<br><br>linecrimson@gmail.com";
+}
+function CardReveal4()
+{
+	document.getElementById("card4").style.height="360px";
+	document.getElementById("card4").innerHTML="TIMING <br><br>OFFICE REMAINS OPEN:<br><br>9:00am-5:00pm<br><br>EVERYDAY";
+	
+}
+function CardReveal5()
+{
+	document.getElementById("card5").style.height="360px";
+	document.getElementById("card5").innerHTML="OFFICE ADRESS<br><br> Building No. 342<br>Sect-05<br> Noida City Center<br> Delhi";
+}
+
+function CardReveal6()
+{
+	document.getElementById("card6").style.height="360px";
+	document.getElementById("card6").innerHTML=" ALSO<br> <br>EVEN THOUGH OUR<br> OFFICES CLOSE AT 5 PM<br> BLOOD REQUESTS AND EMERGENCY REQUESTS<br> ARE STILL HANDLED 24 HOURS.";
+}
